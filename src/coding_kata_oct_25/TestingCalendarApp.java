91d69bcd9b209198cbea1cb685a4f8fcd52d3b74
Samuel Parvu <samuel.parvu.work@gmail.com>
2023-10-25 22:28:16 +0300

    coding kata message

---
+++ src/coding_kata_oct_25/TestingCalendarApp.java
@@ -0,0 +1,13 @@
+package coding_kata_oct_25;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class TestingCalendarApp {
+
+    public static void main(String[] args) {
+        Event event = new Event("Sabi's Wedding", LocalDateTime.now().minusHours(2));
+        event.setDuration(Duration.ofHours(3));
+        System.out.println(event.toString());
+    }
+}
